96fc67a1f6fe6f887d6b53c8fa131527cfc5ea45
daniel206 <buitienduc0966@gmail.com>
2026-06-16 20:53:58 +0700

    Update StarPlayer.java
    lon

--- src/data/StarPlayer.java
+++ src/data/StarPlayer.java
@@ -1,6 +1,7 @@
 package data;
 
 public class StarPlayer extends Player {
+     private static double bonusMultiplier = 500000;
 
     public StarPlayer() {
         super();
@@ -13,9 +14,17 @@ public class StarPlayer extends Player {
             String status) {
         super(playerId, playerType, fullName, age, nationality, position, shirtNumber, baseSalary, status);
     }
+     public static double getBonusMultiplier() {
+        return bonusMultiplier;
+    }
 
+    public static void setBonusMultiplier(double bonusMultiplier) {
+        if (bonusMultiplier > 0) {
+            StarPlayer.bonusMultiplier = bonusMultiplier;
+        }
+    }
     @Override
     public double calculateBonus(int points) {
-        return points * 500000;
+        return points * bonusMultiplier;
     }
 }
